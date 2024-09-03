adb30cbed39690b303f69391fec0bfecbaf2e3c3
IBruteDude <kamarf505@gmail.com>
2024-09-03 12:53:33 +0300

    Annotated the search engine service

--- src/main/java/com/coursecraft/util/SearchEngine.java
+++ src/main/java/com/coursecraft/util/SearchEngine.java
@@ -2,10 +2,13 @@ package com.coursecraft.util;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.coursecraft.dao.AppDao;
 import com.coursecraft.dto.SearchDto;
 import com.coursecraft.entity.Course;
 
+@Service
 public class SearchEngine {
 
 	public List<Course> getSearchResults(AppDao appDao, SearchDto searchDto) {
